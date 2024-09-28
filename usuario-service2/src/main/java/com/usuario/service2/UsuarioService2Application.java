f23d71787ea66d2461aa23aa748f30856144c92c
Alberto Sanchez <albertosanchez15111rgmail.com>
2024-09-28 19:08:20 +0200

    inicio

---
+++ usuario-service2/src/main/java/com/usuario/service2/UsuarioService2Application.java
@@ -0,0 +1,13 @@
+package com.usuario.service2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioService2Application.class, args);
+	}
+
+}
---
+++ usuario-service2/src/test/java/com/usuario/service2/UsuarioService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usuario.service2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
